a1759aec482d259e73889c606277f524bfd4925f
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2025-03-03 04:32:32 +0000

    Add model zoo with weight locations along with segmentation transforms code

---
+++ model/semantic_segmentation/espnetv2/weight_locations.py
@@ -0,0 +1,24 @@
+
+model_weight_map = {}
+# key is of the form <model-name_model-scale>
+
+
+#ESPNetv2
+espnetv2_scales = [0.5, 1.0, 1.5, 2.0]
+for scale in espnetv2_scales:
+    model_weight_map['espnetv2_{}'.format(scale)] = {
+        'pascal_256x256':
+            {
+                'weights': 'model/semantic_segmentation/model_zoo/espnetv2/espnetv2_s_{}_pascal_256x256.pth'.format(scale)
+            },
+        'pascal_384x384':
+            {
+                'weights': 'model/semantic_segmentation/model_zoo/espnetv2/espnetv2_s_{}_pascal_384x384.pth'.format(scale)
+            },
+        'city_1024x512': {
+            'weights': 'model/semantic_segmentation/model_zoo/espnetv2/espnetv2_s_{}_city_1024x512.pth'.format(scale)
+        },
+        'city_512x256': {
+            'weights': 'model/semantic_segmentation/model_zoo/espnetv2/espnetv2_s_{}_city_512x256.pth'.format(scale)
+        }
+    }
