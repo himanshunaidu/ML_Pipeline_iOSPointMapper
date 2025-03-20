6512f941cf37977d1f531d2646a8c1ddd952a043
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2025-03-20 17:29:09 +0000

    Add iou and dice error metrics

--- eval/semantic_segmentation/metrics/dice.py
+++ eval/semantic_segmentation/metrics/dice.py
@@ -1,10 +1,10 @@
 import numpy as np
 import torch
 
-class MIOU(object):
+class Dice(object):
     """
-    Helps to calculate the mean intersection over union (mIoU) metric for semantic segmentation, by providing the
-    intersection and union values for each class.
+    Helps to calculate the Dice coefficient for semantic segmentation, by providing the
+    numerator and denominator values for each class.
     Works with both CPU and GPU PyTorch tensors.
 
     Works with the following assumptions:
@@ -18,13 +18,13 @@ class MIOU(object):
         The number of classes in the dataset.
 
     epsilon : float
-        A small value added to the union to avoid division by zero.
+        A small value added to the denominator to avoid division by zero.
     """
     def __init__(self, num_classes=21, epsilon=1e-6):
         self.num_classes = num_classes
         self.epsilon = epsilon
-
-    def get_iou(self, output, target):
+    
+    def get_dice(self, output, target):
         if isinstance(output, tuple):
             output = output[0]
 
@@ -45,9 +45,9 @@ class MIOU(object):
 
         pred = pred * (target > 0)
         inter = pred * (pred == target)
-        area_inter = torch.histc(inter.float(), bins=self.num_classes, min=1, max=self.num_classes)
+        area_numerator = torch.histc(inter.float(), bins=self.num_classes, min=1, max=self.num_classes)
         area_pred = torch.histc(pred.float(), bins=self.num_classes, min=1, max=self.num_classes)
         area_mask = torch.histc(target.float(), bins=self.num_classes, min=1, max=self.num_classes)
-        area_union = area_pred + area_mask - area_inter + self.epsilon
+        area_denominator = area_pred + area_mask + self.epsilon
 
-        return area_inter.numpy(), area_union.numpy()
+        return area_numerator.numpy() * 2, area_denominator.numpy()
