f8299ddcf102c28cc8f7aa42f936852778bb0aca
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-04-28 17:27:30 -0700

    Use default cityscapes data loader in a test script for reference

--- test_segmentation_with_metrics.py
+++ test_segmentation_with_metrics.py
@@ -121,10 +121,11 @@ def evaluate(args, model, dataset_loader: torch.utils.data.DataLoader, device):
 def main(args):
     # read all the images in the folder
     if args.dataset == 'city':
-        from data_loader.semantic_segmentation.cityscapes import CITYSCAPE_CLASS_LIST
+        from data_loader.semantic_segmentation.cityscapes import CITYSCAPE_CLASS_LIST, CityscapesSegmentation
         from data_loader.semantic_segmentation.backup import CityscapesSegmentationTest
-        dataset = CityscapesSegmentationTest(root=args.data_path, size=args.im_size, scale=args.s,
-                                             coarse=False, split=args.split)
+        dataset = CityscapesSegmentation(root=args.data_path, size=args.im_size, scale=args.s,
+                                             coarse=False, train=(args.split == 'train'),
+                                             mean=[0, 0, 0], std=[1, 1, 1])
         seg_classes = len(CITYSCAPE_CLASS_LIST)
     elif args.dataset == 'edge_mapping': # MARK: edge mapping datasetq
         image_path = os.path.join(args.data_path, "rgb", "*.png")
