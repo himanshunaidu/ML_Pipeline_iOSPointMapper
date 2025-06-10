3635adad68ad0295fd377f138fdec1c1a02212ee
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2025-06-10 06:30:13 +0000

    Additional testing code

--- test_segmentation.py
+++ test_segmentation.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 from utilities.print_utils import *
 from transforms.semantic_segmentation.data_transforms import MEAN, STD
 from utilities.utils import model_parameters, compute_flops
+from data_loader.semantic_segmentation.cityscapes import CITYSCAPE_TRAIN_CMAP
 
 def relabel(img):
     '''
@@ -39,10 +40,10 @@ def relabel(img):
     return img
 
 
-def data_transform(img, im_size):
+def data_transform(img, im_size, mean, std):
     img = img.resize(im_size, Image.BILINEAR)
     img = F.to_tensor(img)  # convert to tensor (values between 0 and 1)
-    img = F.normalize(img, MEAN, STD)  # normalize the tensor
+    img = F.normalize(img, mean, std)  # normalize the tensor
     return img
 
 
@@ -53,6 +54,16 @@ def evaluate(args, model, image_list, device):
     if args.dataset == 'pascal':
         from utilities.color_map import VOCColormap
         cmap = VOCColormap().get_color_map_voc()
+    elif args.dataset == 'city' or args.dataset == 'edge_mapping':
+        cmap = CITYSCAPE_TRAIN_CMAP
+        if args.dataset == 'edge_mapping' and args.is_custom:
+            from data_loader.semantic_segmentation.edge_mapping import edge_mapping_to_custom_cocoStuff_dict
+            cmap = {}
+            for k, v in CITYSCAPE_TRAIN_CMAP.items():
+                if k in edge_mapping_to_custom_cocoStuff_dict.keys():
+                    cmap[edge_mapping_to_custom_cocoStuff_dict[k]] = v
+                else:
+                    cmap[k] = v
     else:
         cmap = None
 
@@ -61,7 +72,7 @@ def evaluate(args, model, image_list, device):
         img = Image.open(imgName).convert('RGB')
         w, h = img.size
 
-        img = data_transform(img, im_size)
+        img = data_transform(img, im_size, args.mean, args.std)
         img = img.unsqueeze(0)  # add a batch dimension
         img = img.to(device)
         img_out = model(img)
@@ -84,8 +95,8 @@ def evaluate(args, model, image_list, device):
         img_out = img_out.resize((w, h), Image.NEAREST)
 
         # pascal dataset accepts colored segmentations
-        if args.dataset == 'pascal':
-            img_out.putpalette(cmap)
+        # if args.dataset == 'pascal':
+        img_out.putpalette(cmap)
 
         # save the segmentation mask
         name = imgName.split('/')[-1]
@@ -97,15 +108,24 @@ def evaluate(args, model, image_list, device):
 def main(args):
     # read all the images in the folder
     if args.dataset == 'city':
-        image_path = os.path.join(args.data_path, "leftImg8bit", args.split, "*", "*.png")
+        # image_path = os.path.join(args.data_path, "leftImg8bit", args.split, "*", "*.png")
+        image_path = os.path.join(args.data_path, "*.jpg")
         image_list = glob.glob(image_path)
         from data_loader.semantic_segmentation.cityscapes import CITYSCAPE_CLASS_LIST
         seg_classes = len(CITYSCAPE_CLASS_LIST)
+    # elif args.dataset == 'edge_mapping': # MARK: edge mapping dataset
+    #     image_path = os.path.join(args.data_path, "rgb", "*.png")
+    #     image_list = glob.glob(image_path)
+    #     from data_loader.semantic_segmentation.edge_mapping import EDGE_MAPPING_CLASS_LIST
+    #     seg_classes = len(EDGE_MAPPING_CLASS_LIST)
     elif args.dataset == 'edge_mapping': # MARK: edge mapping dataset
-        image_path = os.path.join(args.data_path, "rgb", "*.png")
+        from data_loader.semantic_segmentation.edge_mapping import EdgeMappingSegmentation, EDGE_MAPPING_CLASS_LIST, edge_mapping_to_custom_cocoStuff_dict
+        if args.is_custom and args.custom_mapping_dict is None:
+            args.custom_mapping_dict = edge_mapping_to_custom_cocoStuff_dict
+        image_path = os.path.join(args.data_path, "*.jpg")
         image_list = glob.glob(image_path)
-        from data_loader.semantic_segmentation.edge_mapping import EDGE_MAPPING_CLASS_LIST
         seg_classes = len(EDGE_MAPPING_CLASS_LIST)
+        if args.is_custom: seg_classes = 53
     elif args.dataset == 'pascal':
         from data_loader.semantic_segmentation.voc import VOC_CLASS_LIST
         seg_classes = len(VOC_CLASS_LIST)
@@ -131,6 +151,23 @@ def main(args):
         from model.semantic_segmentation.espnetv2.espnetv2 import espnetv2_seg
         args.classes = seg_classes
         model = espnetv2_seg(args)
+        args.mean = MEAN
+        args.std = STD
+    elif args.model == 'bisenetv2':
+        from model.semantic_segmentation.bisenetv2.bisenetv2 import BiSeNetV2
+        seg_classes = seg_classes - 1 if ((args.dataset == 'city' or args.dataset == 'edge_mapping') and not args.is_custom) else seg_classes # Because the background class is not used in the model
+        args.classes = seg_classes
+        model = BiSeNetV2(n_classes=args.classes, aux_mode='eval')
+        # Eventually, the following mean and std related info should be moved to global config
+        if args.dataset == 'city' or args.dataset == 'edge_mapping':
+            args.mean = (0.3257, 0.3690, 0.3223)
+            args.std = (0.2112, 0.2148, 0.2115)
+        elif args.dataset == 'coco_stuff':
+            args.mean = (0.46962251, 0.4464104,  0.40718787)
+            args.std = (0.27469736, 0.27012361, 0.28515933)
+        else:
+            args.mean = MEAN
+            args.std = STD
     else:
         print_error_message('{} network not yet supported'.format(args.model))
         exit(-1)
@@ -144,7 +181,7 @@ def main(args):
     if args.weights_test:
         print_info_message('Loading model weights')
         weight_dict = torch.load(args.weights_test, map_location=torch.device('cpu'))
-        model.load_state_dict(weight_dict)
+        model.load_state_dict(weight_dict, strict=False if args.model == 'bisenetv2' else True)
         print_info_message('Weight loaded successfully')
     else:
         print_error_message('weight file does not exist or not specified. Please check: {}', format(args.weights_test))
@@ -173,6 +210,8 @@ if __name__ == '__main__':
     parser.add_argument('--model-width', default=224, type=int, help='Model width')
     parser.add_argument('--model-height', default=224, type=int, help='Model height')
     parser.add_argument('--channels', default=3, type=int, help='Input channels')
+    parser.add_argument('--is-custom', default=False, type=bool, help='Use custom mapping dictionary')
+    parser.add_argument('--custom-mapping-dict', default=None, type=dict, help='Custom mapping dictionary')
     parser.add_argument('--num-classes', default=1000, type=int,
                         help='ImageNet classes. Required for loading the base network')
 
