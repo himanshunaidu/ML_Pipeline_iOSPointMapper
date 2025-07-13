5829c9519e88ef35e52619c945309b541f0841ee
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2025-07-13 22:35:41 +0000

    Add pydantic based configuration

---
+++ utilities/main_file_utils.py
@@ -0,0 +1,4 @@
+"""
+This utility file contains functions used by the main train and test scripts for various purposes.
+It includes using the arguments provided by the user to get the dataset, the model, optimizer, etc.
+"""
