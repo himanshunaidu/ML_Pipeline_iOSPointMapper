623ebda3d900a1df8ad91f84e4723385d757c41e
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2025-07-14 05:34:17 +0000

    Fix custom maps

--- data_loader/semantic_segmentation/edge_mapping_scripts/custom_maps.py
+++ data_loader/semantic_segmentation/edge_mapping_scripts/custom_maps.py
@@ -10,5 +10,5 @@ edge_mapping_to_cocoStuff_custom_53_dict = {0:41, 1:35, 2:19, 3:50, 4:24, 5:0, 6
 # Mapping from edge mapping classes to **custom** cocostuff classes (35 classes)
 ## This customization of cocostuff classes comes from edge mapping repository
 ## done to map the fewer relevant classes to a continuous range of classes
-edge_mapping_to_cocoStuff_custom_35_dict = {0:27, 1:22, 2:16, 3:33, 4:20, 5:21, 6:8, 7:10, 8:15, 9:19,
-                            10:0, 11:1, 12:1, 13:3, 14:7, 15:5, 16:6, 17:4, 18:2, 19:0}
+edge_mapping_to_cocoStuff_custom_35_dict = {0:26, 1:21, 2:15, 3:32, 4:19, 5:20, 6:7, 7:9, 8:14, 9:18,
+    10:255, 11:0, 12:0, 13:2, 14:6, 15:4, 16:5, 17:3, 18:1, 19:255}
--- data_loader/semantic_segmentation/ios_point_mapper_scripts/custom_maps.py
+++ data_loader/semantic_segmentation/ios_point_mapper_scripts/custom_maps.py
@@ -16,15 +16,15 @@ ios_point_mapper_dict = {
 ## This customization of cocostuff classes comes from edge mapping repository
 ## done to map the fewer relevant classes to a continuous range of classes
 ios_point_mapper_to_cocoStuff_custom_53_dict = {0:0, 1:2, 2:0, 3:0, 4:19, 5:5, 6:3, 7:0, 8:24, 9:0, 10:0, 
-                                      11:2, 12:0, 13:1, 14:0, 15:0, 16:1, 17:41, 18:35, 19:0, 20:0, 
-                                      21:27, 22:8, 23:11, 24:6, 25:12, 26:0, 27:31, 28:50}
+                                    11:2, 12:0, 13:1, 14:0, 15:0, 16:1, 17:41, 18:35, 19:0, 20:0, 
+                                    21:27, 22:8, 23:11, 24:6, 25:12, 26:0, 27:31, 28:50}
 
 # Mapping from ios_point_mapper classes to **custom** cocostuff classes
 ## This customization of cocostuff classes comes from edge mapping repository
 ## done to map the fewer relevant classes to a continuous range of classes
-ios_point_mapper_to_cocoStuff_custom_35_dict = {0:0, 1:2, 2:0, 3:0, 4:16, 5:5, 6:3, 7:0, 8:20, 9:0, 10:25,
-                                      11:4, 12:0, 13:1, 14:21, 15:26, 16:1, 17:27, 18:22, 19:0, 20:0,
-                                      21:19, 22:8, 23:10, 24:6, 25:7, 26:0, 27:15, 28:33}
+ios_point_mapper_to_cocoStuff_custom_35_dict = {0:255, 1:1, 2:255, 3:255, 4:15, 5:4, 6:2, 7:255, 8:19, 9:255,
+                                    10:255, 11:3, 12:255, 13:0, 14:20, 15:255, 16:0, 17:26, 18:21,
+                                    19:255, 20:255, 21:18, 22:7, 23:9, 24:5, 25:6, 26:255, 27:14, 28:32}
 
 # Mapping from ios_point_mapper classes to cityscapes classes
 ios_point_mapper_to_cityscapes_dict = {
